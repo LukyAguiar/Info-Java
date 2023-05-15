f68c187edfdf05447d2c9869499de35e3b00d948
Lucas Aguiar <lucas.nune@outlook.com.br>
2023-05-15 10:43:31 -0300

    Expressoes Aritmeticas - Operações Basicas - Tipos Básicos de Variáveis

---
+++ Estrutura-Sequencial/src/ExpressoesAritmeticas.java
@@ -0,0 +1,31 @@
+public class ExpressoesAritmeticas {
+
+    public static void formatoAritmetico(){
+        System.out.println("- Expressões Aritméticas'");
+        System.out.println(" ( Expressão = 4 + 5 ) - (Resultado =) - (Valor Numérico = 9) ");
+    }
+
+    public static void exemplosAritmeticos(){
+        System.out.println("- Exemplos de expressões aritméticas");
+        System.out.println("2 * 6 / 3 - RESULTADO = 4 (Multiplicação sempre em primeiro)");
+        System.out.println("3 + 2 * 4 - RESULTADO = 11");
+        System.out.println("(3 + 2) * 4 - RESULTADO = 20 (Com o uso do parentese a prioridade muda)");
+        System.out.println("60 / (3 +2 ) * 4 - RESULTADO = 48");
+        System.out.println("60 * ((3+2) * 4) - RESULTADO = 3");
+        System.out.println("-----------------------------------");
+        System.out.println("- Exemplos com o operador 'mod' - Resto da divisão");
+        System.out.println("14 % 3 - RESULTADO = 2");
+        System.out.println("19 % 5 - RESULTADO = 4");
+        System.out.println("-------------------------------");
+    }
+
+    public static void operadoresAritmeticos(){
+        System.out.println("-------------------------------");
+        System.out.println("Operadores | Significado      |");
+        System.out.println("     +     | Adição           |");
+        System.out.println("     -     | Subtração        |");
+        System.out.println("     *     | Multiplicação    |");
+        System.out.println("     /     | Divisão          |");
+        System.out.println("     %     | Resto da divisão |");
+    }
+}
---
+++ Estrutura-Sequencial/src/OperacoesBasicas.java
@@ -0,0 +1,29 @@
+public class OperacoesBasicas {
+
+    public static void entradaDeDados(){
+
+    }
+
+    public static void processamentoDeDados(){
+
+    }
+
+    public static void saidaDeDados(){
+        System.out.println("- Saída de Dados");
+        System.out.println("Para escrever na tela um texto qualquer");
+        System.out.println("Sem quebra de linha ao final: ");
+        System.out.println("System.out.print('Bom dia!');");
+        System.out.println("--------------------------------");
+        System.out.println("Com quebra de linha ao final:");
+        System.out.println("System.out.println('Bom dia!');");
+        System.out.println("--------------------------------");
+        System.out.println("Para escrever o conteúdo de uma variável de algum tipo básico");
+        System.out.println("Suponha que uma variável tipo int decladara e iniciada:");
+        int y = 32;
+        System.out.println("int y = 32;");
+        System.out.println("Após isso utilizamos Sout");
+        System.out.println("System.out.println(y);");
+        System.out.println("Trazendo o valor inserido no y que é");
+        System.out.println(y);
+    }
+}
