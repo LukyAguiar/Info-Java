16ec2c997ad9da4ff1d33aae0fd0e39a682891b4
Lucas Aguiar <lucas.nune@outlook.com.br>
2023-06-02 15:06:39 -0300

    Processamento de dados / Casting

--- Estrutura-Sequencial/src/Main.java
+++ Estrutura-Sequencial/src/Main.java
@@ -7,5 +7,6 @@ public class Main {
         
         System.out.println("Info Java!");
         OperacoesBasicas.saidaDeDados();
+        OperacoesBasicas.exemplosProcessamentoDeDados();
     }
 }
--- Estrutura-Sequencial/src/OperacoesBasicas.java
+++ Estrutura-Sequencial/src/OperacoesBasicas.java
@@ -1,12 +1,82 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class OperacoesBasicas {
 
+    static Scanner sc = new Scanner(System.in);
     public static void entradaDeDados(){
 
     }
 
     public static void processamentoDeDados(){
-
+        System.out.println("- Processamento de dados em Java Casting");
+        System.out.println();
+        System.out.println("REGRA:");
+        System.out.println("1) A expressão é calculada ");
+        System.out.println("2) O resultado da expressão é armazenado na variável ");
+        System.out.println();
+        System.out.println("Comando de atribuição");
+        System.out.println("Sintaxe:");
+        System.out.println("<variável> = <expressão>;");
     }
+
+    public static void exemplosProcessamentoDeDados(){
+        int resp;
+        System.out.println("Qual exemplo deseja ver? [1 a 5]");
+        resp = sc.nextInt();
+
+        switch (resp) {
+            case 1:
+                System.out.println("Exemplo 01");
+                System.out.println();
+                System.out.println("int x, y:");
+                System.out.println("x = 5;");
+                System.out.println("y = 2 * x;");
+                System.out.println("x");
+                System.out.println("y");
+                break;
+            case 2:
+                System.out.println("Exemplo 02");
+                System.out.println("int x;");
+                System.out.println("double y;");
+                System.out.println("x = 5;");
+                System.out.println("y = 2 * x;");
+                System.out.println("System.out.println('x')");
+                System.out.println("System.out.println('y')");
+                break;
+            case 3:
+                System.out.println("Exemplo 03");
+                System.out.println("double B,b,h,area;");
+                System.out.println("B = 8.0;");
+                System.out.println("b = 6.0;");
+                System.out.println("h = 5.0;");
+                System.out.println("area = (b + B) / 2 * h");
+                System.out.println("System.out.println('area')");
+                break;
+            case 4:
+                System.out.println("Exemplo 04");
+                System.out.println("int a,b;");
+                System.out.println("double resultado;");
+                System.out.println();
+                System.out.println("a = 5;");
+                System.out.println("b = 2;");
+                System.out.println();
+                System.out.println("resultado = (double) a / b;");
+                System.out.println("System.out.println('resultado')");
+                break;
+            case 5:
+                System.out.println("Exemplo 05");
+                System.out.println("double a;");
+                System.out.println("int b;");
+                System.out.println("a = 5.0");
+                System.out.println("b = a;");
+                System.out.println("System.out.println('b')");
+                break;
+            default:
+                System.out.println("Número invalido, digite outro:");
+        }
+    }
+
 
     public static void saidaDeDados(){
         System.out.println("- Saída de Dados");
